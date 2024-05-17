bd321144c64fa4a87b160ad719ac7523ce7ae720
rickherrera3001 <rihr.3001@gmail.com>
2024-05-17 00:50:53 -0600

    create model

--- FantasyRoleGnomo/Model/Model.swift
+++ FantasyRoleGnomo/Model/Model.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+
+import Foundation
+
+// MARK: - Fantasyrole
+struct Fantasyrole: Codable {
+    let brastlewark: [Brastlewark]
+
+    enum CodingKeys: String, CodingKey {
+        case brastlewark = "Brastlewark"
+    }
+}
+
+// MARK: - Brastlewark
+struct Brastlewark: Codable {
+    let id: Int
+    let name: String
+    let thumbnail: String
+    let age: Int
+    let weight, height: Double
+    let hairColor: HairColor
+    let professions: [Profession]
+    let friends: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, thumbnail, age, weight, height
+        case hairColor = "hair_color"
+        case professions, friends
+    }
+}
+
+enum HairColor: String, Codable {
+    case black = "Black"
+    case gray = "Gray"
+    case green = "Green"
+    case pink = "Pink"
+    case red = "Red"
+}
+
+enum Profession: String, Codable {
+    case baker = "Baker"
+    case blacksmith = "Blacksmith"
+    case brewer = "Brewer"
+    case butcher = "Butcher"
+    case carpenter = "Carpenter"
+    case cook = "Cook"
+    case farmer = "Farmer"
+    case gemcutter = "Gemcutter"
+    case leatherworker = "Leatherworker"
+    case marbleCarver = "Marble Carver"
+    case mason = "Mason"
+    case mechanic = "Mechanic"
+    case medic = "Medic"
+    case metalworker = "Metalworker"
+    case miner = "Miner"
+    case potter = "Potter"
+    case prospector = "Prospector"
+    case sculptor = "Sculptor"
+    case smelter = "Smelter"
+    case stonecarver = "Stonecarver"
+    case tailor = "Tailor"
+    case taxInspector = "Tax inspector"
+    case tinker = " Tinker"
+    case woodcarver = "Woodcarver"
+}
+
