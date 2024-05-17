04f6db5abe5884b3d2779c61c3c1b6862757d0a3
rickherrera3001 <rihr.3001@gmail.com>
2024-05-17 01:10:46 -0600

    create apiClient

--- FantasyRoleGnomo/Network/ApiClient.swift
+++ FantasyRoleGnomo/Network/ApiClient.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class ApiClient {
+    
+    func getData(completion: @escaping(Result<Fantasyrole, Error>) -> Void) {
+        var request = URLRequest(url: URL(string: "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json")!, timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request){
+            data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            print(String(data: data, encoding: .utf8)!)
+            
+            do {
+                let result = try JSONDecoder().decode(Fantasyrole.self, from: data)
+                completion(.success(result))
+            } catch{
+                print("Error decoding JSON: \(error)")
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+}
