860dfcdd652faf0cb3411e419d1f730cabf3dec9
rickherrera3001 <rihr.3001@gmail.com>
2024-05-17 00:40:50 -0600

    Initial Commit

---
+++ FantasyRoleGnomo/FantasyRoleGnomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyRoleGnomoApp.swift
+//  FantasyRoleGnomo
+//
+//  Created by Ricardo Developer on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FantasyRoleGnomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
